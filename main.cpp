91d1c298b185e83e9633611cf09c40914ad1e34b
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2023-03-21 14:32:53 +0100

    fix: latest transcode

--- main.cpp
+++ main.cpp
@@ -27,12 +27,6 @@ int custom_io_write(void *opaque, uint8_t *buffer, int32_t buffer_size)
   return buffer_size;
 }
 
-<<<<<<< Updated upstream
-extern "C"
-{
-  EMSCRIPTEN_KEEPALIVE
-  int encode_mux(uint8_t *array, const int size, const int sample_rate, const int num_channels, const int profile_id)
-=======
 struct buffer_data
 {
   uint8_t *ptr;
@@ -95,47 +89,10 @@ extern "C"
   };
   EMSCRIPTEN_KEEPALIVE
   Result *transcode(uint8_t *array, const size_t size, const char *src_format_short_name, const char *dst_format_short_name, const char *dst_codec_name)
->>>>>>> Stashed changes
   {
-    //
-    const int pcm_data_size = size;
-    //
-    const AVSampleFormat input_format = AV_SAMPLE_FMT_S16;
-    const int codec_id = profile_id == 0   ? AV_CODEC_ID_AAC
-                         : profile_id == 1 ? AV_CODEC_ID_MP2
-                         : profile_id == 2 ? AV_CODEC_ID_OPUS
-                         : profile_id == 3 ? AV_CODEC_ID_MP3
-                         : profile_id == 4 ? AV_CODEC_ID_OPUS
-                                           : NULL;
-    const char *format_short_name = profile_id == 0   ? "adts"
-                                    : profile_id == 1 ? "mp2"
-                                    : profile_id == 2 ? "webm"
-                                    : profile_id == 3 ? "mp3"
-                                    : profile_id == 4 ? "ogg"
-                                                      : nullptr;
-    printf("%s\n", format_short_name);
-    const int out_sample_rate = 16000;
-    const int out_bit_rate = 32000;
-    //
-    const AVOutputFormat *out_fmt = throw_if_null(av_guess_format(format_short_name, NULL, NULL), "Output format not recognized");
-    AVFormatContext *format_ctx = nullptr;
-    throw_if_neg(avformat_alloc_output_context2(&format_ctx, out_fmt, "wav", nullptr), "Failed to allocate output context");
-    const AVCodec *codec = throw_if_null(avcodec_find_encoder(static_cast<AVCodecID>(codec_id)), "Codec not found");
-    AVStream *stream = throw_if_null(avformat_new_stream(format_ctx, codec), "Failed to create a new stream");
-    AVCodecContext *codec_ctx = throw_if_null(avcodec_alloc_context3(codec), "Failed to allocate codec context");
-    codec_ctx->strict_std_compliance = -2;
-    AVChannelLayout src_ch_layout;
-    (num_channels == 1) ? src_ch_layout = AV_CHANNEL_LAYOUT_MONO : src_ch_layout = AV_CHANNEL_LAYOUT_STEREO;
-    throw_if_neg(av_channel_layout_copy(&codec_ctx->ch_layout, &src_ch_layout), "Could not select channel layout");
-    codec_ctx->bit_rate = out_bit_rate;
-    codec_ctx->sample_rate = out_sample_rate;
-    codec_ctx->sample_fmt = codec->sample_fmts[0];
-    codec_ctx->time_base = (AVRational){1, out_sample_rate};
-    throw_if_neg(avcodec_open2(codec_ctx, codec, nullptr), "Failed to open codec");
-    throw_if_neg(avcodec_parameters_from_context(stream->codecpar, codec_ctx), "Failed to copy codec parameters");
+    printf("dst_format_short_name: %s\n", dst_format_short_name);
+    printf("dst_codec_name: %s\n", dst_codec_name);
 
-<<<<<<< Updated upstream
-=======
     // Create input AVIOContext
     struct buffer_data bd = {array, size};
     std::vector<uint8_t> input_buffer(1);
@@ -178,66 +135,54 @@ extern "C"
     throw_if_neg(avcodec_parameters_from_context(out_stream->codecpar, enc_ctx), "Failed to copy destination codec parameters");
 
     // Create a custom IO context for writing the output
->>>>>>> Stashed changes
     std::vector<uint8_t> output_buffer(1);
-    AVIOContext *avio_ctx = avio_alloc_context(output_buffer.data(), output_buffer.size(), 1, nullptr, nullptr, &custom_io_write, nullptr);
-    format_ctx->pb = avio_ctx;
-    format_ctx->flags |= AVFMT_FLAG_CUSTOM_IO;
+    AVIOContext *output_avio_ctx = avio_alloc_context(output_buffer.data(), output_buffer.size(), 1, nullptr, nullptr, &custom_io_write, nullptr);
+    output_ctx->pb = output_avio_ctx;
+    output_ctx->flags |= AVFMT_FLAG_CUSTOM_IO;
 
-    throw_if_neg(avformat_write_header(format_ctx, nullptr), "Failed to write header");
+    // Write header
+    throw_if_neg(avformat_write_header(output_ctx, nullptr), "Failed to write header");
 
-    AVFrame *frame = av_frame_alloc();
-    throw_if_null(frame, "Failed to allocate frame");
-
-    frame->nb_samples = codec_ctx->frame_size;
-    frame->format = codec_ctx->sample_fmt;
-    throw_if_neg(av_channel_layout_copy(&frame->ch_layout, &codec_ctx->ch_layout), "Could not copy channel layout");
-
-    throw_if_neg(av_frame_get_buffer(frame, 0), "Failed to allocate frame data");
-
+    // Prepare resampler
     SwrContext *swr_ctx = nullptr;
     swr_alloc_set_opts2(&swr_ctx,
-                        &codec_ctx->ch_layout, codec_ctx->sample_fmt, codec_ctx->sample_rate,
-                        &codec_ctx->ch_layout, input_format, sample_rate,
-                        0, nullptr);
+                        &enc_ctx->ch_layout, enc_ctx->sample_fmt, enc_ctx->sample_rate,
+                        &dec_ctx->ch_layout, dec_ctx->sample_fmt, dec_ctx->sample_rate, 0, nullptr);
+    swr_set_compensation(swr_ctx, (enc_ctx->frame_size - dec_ctx->frame_size) * enc_ctx->sample_rate / dec_ctx->sample_rate, enc_ctx->frame_size);
     throw_if_null(swr_ctx, "Failed to allocate resampler context");
     throw_if_neg(swr_init(swr_ctx), "Failed to initialize resampler context");
 
-    int frame_size_bytes = (frame->nb_samples * num_channels * av_get_bytes_per_sample(static_cast<AVSampleFormat>(frame->format)));
-    int pos = 0;
-    int ret;
-    while (pos + frame_size_bytes <= pcm_data_size)
+    // Transcode audio
+    AVFrame *frame = av_frame_alloc();
+    AVPacket *pkt = throw_if_null(av_packet_alloc(), "av_packet_alloc failed");
+
+    while (true)
     {
-      const uint8_t *tmp_ptr = array + pos;
-      throw_if_neg(swr_convert(swr_ctx, frame->data, frame->nb_samples,
-                               &tmp_ptr, frame->nb_samples),
-                   "Failed to convert samples");
+      // reads an audio packet from a demuxer (muxer1/input_ctx) and stores it in an AVPacket pkt object.
+      int ret = av_read_frame(input_ctx, pkt);
+      if (ret == AVERROR_EOF)
+        break;
+      throw_if_neg(ret, "Failed to read frame");
 
-      frame->pts = pos / (num_channels * av_get_bytes_per_sample(input_format));
-
-      AVPacket *pkt = throw_if_null(av_packet_alloc(), "av_packet_alloc");
-      pkt->data = nullptr;
-      pkt->size = 0;
-      throw_if_neg(avcodec_send_frame(codec_ctx, frame), "Failed to send frame");
-      while (true)
+      // checks whether the current packet belongs to the same stream as the one being processed
+      if (pkt->stream_index != in_stream->index)
       {
-        const int ret = avcodec_receive_packet(codec_ctx, pkt);
-        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
-          break;
-<<<<<<< Updated upstream
-        throw_if_neg(ret, "Failed to receive packet.");
-        av_packet_rescale_ts(pkt, codec_ctx->time_base, stream->time_base);
-        pkt->stream_index = stream->index;
-        throw_if_neg(av_write_frame(format_ctx, pkt), "Failed to write frame");
         av_packet_unref(pkt);
+        continue;
       }
 
-      pos += frame_size_bytes;
-    }
+      // Sends an encoded packet from the demuxer (muxer1) to the decoder for decoding.
+      // The decoder uses this function to pass the compressed data to the decoding pipeline.
+      // dec_ctx <-- pkt (from input)
+      throw_if_neg(avcodec_send_packet(dec_ctx, pkt), "Failed to send packet to decoder");
 
-    throw_if_neg(av_write_trailer(format_ctx), "Failed to write trailer");
-
-=======
+      while (true)
+      {
+        // receives a decoded frame from the decoder, contains uncompressed audio
+        // frame <-- dec_ctx
+        ret = avcodec_receive_frame(dec_ctx, frame);
+        if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
+          break;
         throw_if_neg(ret, "Failed to receive frame from decoder");
         AVFrame *out_frame = av_frame_alloc();
         out_frame->nb_samples = enc_ctx->frame_size;
@@ -296,19 +241,10 @@ extern "C"
     r->sample_rate = enc_ctx->sample_rate;
 
     // Cleanup
->>>>>>> Stashed changes
     swr_free(&swr_ctx);
     av_frame_free(&frame);
-    avcodec_free_context(&codec_ctx);
-    avformat_free_context(format_ctx);
+    av_packet_free(&pkt);
 
-<<<<<<< Updated upstream
-    output_buffer.resize(avio_ctx->pos);
-    av_free(avio_ctx);
-
-    std::memcpy(array, result.data(), result.size());
-    return result.size();
-=======
     avcodec_close(dec_ctx);
     avcodec_free_context(&dec_ctx);
     avcodec_close(enc_ctx);
@@ -320,6 +256,5 @@ extern "C"
     avformat_close_input(&output_ctx);
     avformat_free_context(output_ctx);
     return r;
->>>>>>> Stashed changes
   }
 }
--- mod.ts
+++ mod.ts
@@ -5,9 +5,45 @@ import { addWaveHeader } from "https://deno.land/x/wave_header/mod.ts";
 const wasmPath =
   "https://raw.githubusercontent.com/bashi/minimp3-wasm/master/dist/decoder.opt.wasm";
 
-export const profiles = { AAC: 0, MP2: 1, WEBM_OPUS: 2, MP3: 3, OGG_OPUS: 4 };
+const createMalloc = (handler) => {
+  const cleanSet = [];
+  return [
+    (arrOrString) => {
+      const inputArray =
+        typeof arrOrString === "string"
+          ? new TextEncoder().encode(arrOrString + "\0")
+          : arrOrString;
+      const size = inputArray.length;
+      const ptr = handler._malloc(size);
+      handler.HEAPU8.set(inputArray, ptr);
+      cleanSet.push(() => handler._free(ptr));
+      return [ptr, size];
+    },
+    () => cleanSet.forEach((fn) => fn()),
+  ];
+};
 
-export const run = async (
+export const probe = async (avArray: Uint8Array, sourceContainer: string) => {
+  const handler = await ModuleFactory({ INITIAL_MEMORY: 128 * 1024 * 1024 });
+  const [malloc, free] = createMalloc(handler);
+  const [avArrayPtr, size] = malloc(avArray);
+  const [sourceContainerPtr] = malloc(sourceContainer);
+  const structPtr = handler._probe(avArrayPtr, size * 2, sourceContainerPtr);
+  const view = new DataView(handler.HEAPU8.buffer, structPtr, 12);
+  const res = {
+    size: view.getUint16(0, true),
+    sampleRate: view.getUint16(2, true),
+    bitDepth: view.getUint16(4, true),
+    numChannels: view.getUint16(6, true),
+    duration: view.getUint16(8, true),
+    frameSize: view.getUint16(10, true),
+  };
+  console.log(res);
+  free();
+  return res;
+};
+
+export const transcode = async (
   avArray: Uint8Array,
   sourceContainer: string,
   destContainer: string,
@@ -20,21 +56,24 @@ export const run = async (
   const [sourceContainerPtr] = malloc(sourceContainer);
   const [destContainerPtr] = malloc(destContainer);
   const [destCodecPtr] = malloc(destCodec);
-  const modifiedArray = new Uint8Array(
-    new Uint8Array(
-      handler.HEAPU8.buffer,
-      avArrayPtr,
-      handler._transcode(
-        avArrayPtr,
-        size * 2,
-        sourceContainerPtr,
-        destContainerPtr,
-        destCodecPtr
-      )
-    )
+
+  const structPtr = handler._transcode(
+    avArrayPtr,
+    size * 2,
+    sourceContainerPtr,
+    destContainerPtr,
+    destCodecPtr
+  );
+  const view = new DataView(handler.HEAPU8.buffer, structPtr, 4);
+  const res = {
+    size: view.getUint16(0, true),
+    sample_rate: view.getUint16(2, true),
+  };
+  const data = new Uint8Array(
+    new Uint8Array(handler.HEAPU8.buffer, avArrayPtr, res.size)
   );
   free();
-  return modifiedArray;
+  return { data, ...res };
 };
 
 if (import.meta.main) {
@@ -45,17 +84,31 @@ if (import.meta.main) {
     )
   )
     .then((r) => r.arrayBuffer())
-    .then((v) => new Uint8Array(v));
-  await run(data, "wav", "adts", "libfdk_aac").then((res) =>
-    Deno.writeFile("./dist/out.aac", res)
+    .then((v) =>
+      addWaveHeader(new Uint8Array(v).slice(0, 2 * 1024 * 1024), 2, 48_000, 16)
+    );
+  // .then(async (arr) => {
+  //   const decoder = await createDecoder(arr, wasmPath);
+  //   const { pcm, samplingRate, numChannels } = decoder.decode(
+  //     decoder.duration
+  //   );
+  //   return addWaveHeader(
+  //     new Uint8Array(pcm.buffer),
+  //     numChannels,
+  //     samplingRate,
+  //     16
+  //   );
+  // });
+  await transcode(data, "wav", "adts", "libfdk_aac").then(({ data }) =>
+    Deno.writeFile("./dist/out.aac", data)
   );
-  await run(data, "wav", "ogg", "libopus").then((res) =>
-    Deno.writeFile("./dist/out.ogg", res)
-  );
-  await run(data, "wav", "mp2", "mp2").then((res) =>
-    Deno.writeFile("./dist/out.mp2", res)
-  );
-  await run(data, "wav", "webm", "libopus").then((res) =>
-    Deno.writeFile("./dist/out.webm", res)
-  );
+  // await transcode(data, "wav", "ogg", "libopus").then(({ data }) =>
+  //   Deno.writeFile("./dist/out.ogg", data)
+  // );
+  // await transcode(data, "wav", "mp2", "mp2").then(({ data }) =>
+  //   Deno.writeFile("./dist/out.mp2", data)
+  // );
+  // await transcode(data, "wav", "webm", "libopus").then(({ data }) =>
+  //   Deno.writeFile("./dist/out.webm", data)
+  // );
 }
